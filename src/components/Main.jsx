b5c82326d2499c49573d2b1a070d7d172eff02f4
dhavamani16 <dhavamani17092005@gmail.com>
2026-01-07 13:16:07 +0530

    Initial React + Vite setup

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+function Main(){
+    return(
+        <main>
+        <h1>Fun facts about React!</h1>
+        <ul className="fir">
+            <li>Was first release in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+
+        </ul>
+        </main>
+    )
+}export default Main
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+    return (
+        <header>
+            <nav>
+            <img src="./react-logo.png" alt="Logo" width="40"/>
+            <h1>ReactFacts</h1>
+            </nav>
+        </header>
+    )
+}
+export default Navbar
