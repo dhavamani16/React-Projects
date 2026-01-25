cade76fdfecc86062ceff91d1c35428d70a9da22
Dhavamani A <dhavamani17092005@gmail.com>
2026-01-25 19:45:33 +0530

    Add IngredientsList component for displaying ingredients

---
+++ my-react-app/src/components/IngredientsList.jsx
@@ -0,0 +1,22 @@
+export default function IngredientsList( { ingredientsListItems, ingredients, handleGetRecipe, loading } ) {
+    return (
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">
+            {ingredientsListItems}
+          </ul>
+
+          {ingredients.length > 3 && (
+            <div className="get-recipe-container">
+              <div>
+                <h3>Ready for a recipe?</h3>
+                <p>Generate a recipe from your list of ingredients.</p>
+              </div>
+              <button onClick={handleGetRecipe} disabled={loading}>
+                {loading ? "Generating..." : "Get a recipe"}
+              </button>
+            </div>
+          )}
+        </section>
+    )
+}
